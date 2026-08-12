2e5235f625d6f101903899875d6e47191c2cfef8
Yahyaball <questkrok123@gmail.com>
2026-08-12 23:05:07 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const qna = document.querySelectorAll(".qna");
+
+qna.forEach((q) => {
+  const button = q.querySelector(".question");
+  const answer = q.querySelector(".answer");
+  let buttonIcon = button.querySelector(".button-icon");
+  q.addEventListener("click", () => {
+    if (answer.classList.contains('show')) {
+        answer.classList.remove('show');
+        answer.classList.add('hide');
+        buttonIcon.setAttribute('src', 'assets/images/icon-plus.svg')
+
+    } else {
+        answer.classList.remove('hide');
+        answer.classList.add('show');
+        buttonIcon.setAttribute('src', 'assets/images/icon-minus.svg')
+    }
+  });
+});
